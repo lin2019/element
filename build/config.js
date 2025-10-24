ba061955d9d4c0b69248efa91164a4d8f671c75e
ljx <1092533307@qq.com>
2025-10-24 16:59:52 +0800

    fix

--- build/config.js
+++ build/config.js
@@ -8,22 +8,22 @@ var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
 var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'));
 var externals = {};
 
-Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `@yq-ui/code/lib/${key}`;
+Object.keys(Components).forEach(function (key) {
+  externals[`element-ui/packages/${key}`] = `yq-ui-code/lib/${key}`;
 });
 
-externals['element-ui/src/locale'] = '@yq-ui/code/lib/locale';
-utilsList.forEach(function(file) {
+externals['element-ui/src/locale'] = 'yq-ui-code/lib/locale';
+utilsList.forEach(function (file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `@yq-ui/code/lib/utils/${file}`;
+  externals[`element-ui/src/utils/${file}`] = `yq-ui-code/lib/utils/${file}`;
 });
-mixinsList.forEach(function(file) {
+mixinsList.forEach(function (file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `@yq-ui/code/lib/mixins/${file}`;
+  externals[`element-ui/src/mixins/${file}`] = `yq-ui-code/lib/mixins/${file}`;
 });
-transitionList.forEach(function(file) {
+transitionList.forEach(function (file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `@yq-ui/code/lib/transitions/${file}`;
+  externals[`element-ui/src/transitions/${file}`] = `yq-ui-code/lib/transitions/${file}`;
 });
 
 externals = [Object.assign({
